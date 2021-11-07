a814cd57c13ce7584f108a398d66b3cd9ec5e578
xovest <79008917+xovest@users.noreply.github.com>
2021-11-07 20:47:23 +0200

    confirm file done

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Confirm from './Confirm';
 import FormPersonalDetails from './FormPersonalDetails';
 import FormUserDetails from './FormUserDetails';
 
@@ -60,7 +61,13 @@ export class UserForm extends Component {
         );
       
       case 3:
-        return <h1>conf</h1>
+        return (
+          <Confirm 
+            nextStep={this.nextStep}
+            prevStep={this.prevStep}
+            values={values}
+          />
+        );
 
       case 4:
         return <h1>success.</h1>
